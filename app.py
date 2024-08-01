de6fc71240ca8d48d881167ba9ea1708066e69cc
GuilherDeMedeirosBif <guilhermedemedeiros331@gmail.com>
2024-08-01 16:11:29 -0300

    Botões

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('teste')
+import pyautogui
+from time import sleep
+
+#Botões de click:
+click_1 = 'click-1.png'
+click_5 = 'click-5.png'
+click_100 = 'click-100.png'
+click_9k = 'click-9k.png'
+click_25k = 'click-25k.png'
+click_100k = 'click-100k.png'
